19edc7748a45149ffc74e27537246d1367e2f248
AkkalaNikhitha <102645886+AkkalaNikhitha@users.noreply.github.com>
2022-05-12 12:13:24 +0530

    Create Gallery.js

---
+++ Gallery.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Gallery.css'
+
+function Gallery() {
+  return(
+    <div class="c">
+      <img src="https://www.macraerentals.com.au/wp-content/uploads/2017/01/shutterstock_259616018.jpg" width="220px" height="180px"/>
+      <img src="https://th.bing.com/th/id/R.1cb5345aa41c599875bf03a26ae9bef6?rik=cwd4CECzNAjPEA&riu=http%3a%2f%2fetsconnect.com%2fwp-content%2fuploads%2f2017%2f06%2fWifi-Education-1.jpg&ehk=1npPtlJyKeg9JFRfDiXXxF9SaoiVft3N5q5s05Jn%2fA0%3d&risl=&pid=ImgRaw&r=0" width="220px" height="180px"/>
+      <img src="https://familylivingtoday.com/wp-content/uploads/2017/11/A-group-of-kids-using-a-laptop.jpg" width="220px" height="180px"/>
+      <img src="https://i2.wp.com/www.additudemag.com/wp-content/uploads/2016/11/Treat.Kids_.Best_software_and_gadgets_for_ADHD_students.Article.6585A.girl_laptop.ts_474867580-3.jpg?resize=1280%2C720px&ssl=1" width="220px" height="180px" />
+      <img src="https://i2-prod.chroniclelive.co.uk/incoming/article7718363.ece/ALTERNATES/s1227b/JS44834556.jpg" width="220px" height="180px" />
+      <img src="https://thumbs.dreamstime.com/z/group-school-kids-tablet-pc-classroom-education-elementary-learning-technology-people-concept-computer-having-fun-50428569.jpg" width="220px" height="180px" />
+      <img src="https://thumbs.dreamstime.com/z/school-boy-using-computer-young-happy-schoolboy-to-search-internet-arab-child-learning-use-elementary-portrait-smiling-115138405.jpg" width="220px" height="180px" />
+      <img src="https://tse3.mm.bing.net/th/id/OIP.0Vlyi1zXbxUEOCT3zy1hYwHaE8?pid=ImgDet&rs=1" width="220px" height="180px" />
+    </div>
+  )
+}
+export default Gallery;
