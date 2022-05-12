49a796f905935d0daa30c8f49a845a96e4c37f36
AkkalaNikhitha <102645886+AkkalaNikhitha@users.noreply.github.com>
2022-05-12 12:08:42 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,32 @@
+import  React from "react";
+import './About.css';
+
+function About() {
+  return(
+      <div className="about-head">
+      <div className="inside1">
+      <h3>Enriching and Fun Early Years Matter Preschool Curriculum</h3>
+      <p>Our curriculum focuses on the holistic development of the child and is suitably mapped to age-appropriate objectives and outcomes. Based on Dr. HowardGardner’s Theory of Multiple Intelligences and Experiential Learning, the KLAY curriculum empowers the child to be able to establish a routine, follow
+      directions, make friends and work independently.</p>
+      </div>
+      <div className="round1">
+      <img src="https://info.athenacareers.edu/hs-fs/hubfs/bigstock-Asian-Female-Teacher-Teaching--268457023.jpg?width=650&name=bigstock-Asian-Female-Teacher-Teaching--268457023.jpg"  />
+      </div>
+      <div className="round2">
+      <img src="https://3kyfma1tr3gd273qz5237tyv-wpengine.netdna-ssl.com/wp-content/uploads/2021/05/n2SaXx6oHs-1-768x512.jpg"  />
+      </div>
+      <div className="inside2">
+      <h3>Socio-emotional & Physical Development</h3>
+      <p>A multi-sensory environment that includes group activities, role plays, story time and drama that have been curated to meet the child's social, emotional, physical and emotional milestones.We make them 'Big School Ready'.</p>
+      </div>
+     <div className="inside3">
+     <h3>Milestones Tracking and Regular Assessments</h3>
+      <p>180 degree evaluation, milestone tracking and regular assessments every term, to let you know of your child’s progress. We also help counsel with strategies, should a child require a more focused stimulation.</p>
+      </div>
+      <div className="round3">
+      <img src="https://www.med-health.net/images/10448022/image001.jpg" />
+    </div>
+    </div>
+  )
+}
+export default About;
