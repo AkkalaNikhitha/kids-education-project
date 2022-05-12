908756894c5d48f8a1189c79b2378dca7ecd17a2
AkkalaNikhitha <102645886+AkkalaNikhitha@users.noreply.github.com>
2022-05-12 12:14:55 +0530

    Create Pages.js

---
+++ Pages.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './Pages.css';
+
+function Pages(){
+  return(
+    <div className="container">
+    <ul className="n">
+      <li>Frond end!</li>
+      <li>Back end!</li>
+      <li>Database!</li>
+      <li>Career path</li>
+      </ul>
+    <h3 className="t">Benefits of Working at KLAY</h3>
+    <p>At KLAY, We believe, Happy Employees = Happy Children = Happy Customers!
+    Therefore, we provide benefits beyond the basic necessities like medical insurance & flexibility that support a balanced life.</p>
+<ul className="v">
+  <li>Staff Child Care </li>
+  <li>Medical Insurance/ESI </li>
+  <li>Provident Fund </li>
+  <li>Annual Bonus </li>
+  <li>Attendance</li>
+  </ul>
+  <p>In order to enable employees to bring their children to work and provide them with world class education, we have an employee-child education policy in place. Employees with young children who fit into the age group and schooling programmes offered in the Centre they are working at, can enrol their
+child in to their age specific programmes. The fee for the schooling/day care program will be subsidised.</p>
+</div>
+  )
+}
+export default Pages;
